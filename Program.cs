3d0502c5fdced70a639cf984366ccd37a38fb559
Julian Velasco <104031876+julian0545465@users.noreply.github.com>
2022-07-06 20:57:12 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace First_proyect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("VARIABLES");
+            //Console.WriteLine("");
+            //Console.WriteLine("What is your name?: ");
+            //string fullName = Console.ReadLine();
+            //Console.WriteLine("");
+            //Console.WriteLine("Hello, " + fullName + " Welcome to this program");
+            ////Console.WriteLine("-----------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------
+
+            //Console.WriteLine("VARIABLES 2");
+            //int n1;
+            //int n2;
+            //Console.WriteLine("Input the first number");
+            //n1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Input the second number");
+            //n2 = Convert.ToInt32(Console.ReadLine());
+
+            //int total = n1 * n2;
+            //Console.WriteLine("");
+            //Console.WriteLine("The result is: " + total);
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------
+
+            //Console.WriteLine("MANEJO DE STRINGS");
+            //double height = 1.70;
+            //int age = 19;
+            //string name = "Julian Velasco";
+
+            //string information = $"The requested info is:\nUser name: {name} \nUser age: {age} \nUser height: {height}";
+            //Console.WriteLine("");
+            //Console.WriteLine(information);
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------
+
+            //Console.WriteLine("TRABAJANDO CON NÚMEROS Y OPERADORES ARITMÉTICOS");
+            //float side1 = 10.2f;
+            //int side2 = 20;
+            //side2++;
+            //side2++;
+            //side2++;
+            //side2--;
+
+            //float area = side1 * side2;
+            //Console.WriteLine("");
+            //Console.WriteLine("The rectangle area is: " + area);
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------
+
+            //Console.WriteLine("OPERADORES LÓGICOS");
+            //Console.WriteLine("");
+            //bool value1 = true;
+            //bool value2 = true;
+            //bool value3 = false;
+
+            //bool result = value1 && value2 && value3;
+            //Console.WriteLine("The result of logical AND is: " + result);
+
+            //bool result2 = value1 || value2 || value3;
+            //Console.WriteLine("The result of logical OR is: " + result2);
+
+            //bool result3 = !value1;
+            //Console.WriteLine("The result of logical NOT (!) is: " + result3);
+
+            //bool result4 = value1 ^ value2;
+            //Console.WriteLine("The result of logical XOR is: " + result4);
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------
+
+            //Console.WriteLine("OPERADORES RELACIONALES");
+            //Console.WriteLine("");
+            //int value1 = 18;
+            //int value2 = 18;
+            //int value3 = -18;
+
+            //bool result0 = value1 == value2;
+            //Console.WriteLine("The result of value 1 == value2 is: " + result0);
+
+            //bool result1 = value1 != value2;
+            //Console.WriteLine("The result of value 1 != value2 is: " + result1);
+
+            //bool result2 = value1 > value2;
+            //Console.WriteLine("The result of value 1 > value2 is: " + result2);
+
+            //bool result3 = value1 < value2;
+            //Console.WriteLine("The result of value 1 < value2 is: " + result3);
+
+            //bool result4 = value1 >= value2;
+            //Console.WriteLine("The result of value 1 >= value2 is: " + result4);
+
+            //bool result5 = value1 <= value2;
+            //Console.WriteLine("The result of value 1 <= value2 is: " + result5);
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------
+
+            //Console.WriteLine("LEER DATOS DEL USUARIO");
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Please enter the side A of the rectangle, you can use decimals");
+            //float sideA = float.Parse(Console.ReadLine());
+            ////float sideA = Convert.ToSingle(Console.ReadLine());
+
+            //Console.WriteLine("Please enter the side B of the rectangle, you can use decimals");
+            //float sideB = float.Parse(Console.ReadLine());
+
+            //float area = sideA * sideB;
+
+            //Console.WriteLine("The rectangle area is: " + area);
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------;
+
+            //Console.WriteLine("ARREGLOS");
+            //Console.WriteLine("");
+
+            //string[] coffeTypes;
+            //float[] coffePrices;
+
+            ////Declaramos los valores que tendremos en los arrays (arreglos)
+            //coffeTypes = new string[] { "Expresso", "Capuccino", "Largo", "Filtrado", "Latte" };
+            //coffePrices = new float[] { 1.2f, 2.0f, 3.0f, 5.0f, 5.5f };
+
+            ////Cambiando valor de un elemento del string
+            ////Accedemos a el valor 1 de nuestro array coffeTypes, y modificamos ingresando el nuevo valor
+            //coffeTypes[1] = "Lungo";
+            //coffeTypes[4] = "Normal Coffe";
+
+            ////Mientras i sea menor que la cantidad de datos en coffetypes
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    //Imprime los elementos de nuestro string concatenando los valores flotantes del segundo string
+
+            //    Console.WriteLine(coffeTypes[i] + ", Coffe $:" + coffePrices[i]);
+            //}
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------;
+
+            //Console.WriteLine("LISTAS");
+            //Console.WriteLine("");
+
+            //List<string> tacoShoppingList = new List<string>();
+
+            //tacoShoppingList.Add("Cinco Tacos de suadero");
+            //tacoShoppingList.Add("Cuatro Tacos de tripa");
+            //tacoShoppingList.Add("Cinco Tacos de pastor");
+            //tacoShoppingList.Add("Cuatro Coca Colas");
+
+
+            //Console.WriteLine("\nHello, Tacos JC!\n");
+            //for (int i = 0; i < tacoShoppingList.Count; i++) {
+            //    Console.WriteLine(".Product: " + tacoShoppingList[i]);
+            //}
+
+
+            //tacoShoppingList.Add("Cinco Cervezas");
+            //tacoShoppingList.Remove("Cinco Tacos de suadero");
+            //Console.WriteLine("");
+            //Console.WriteLine("\nHello, Tacos LJ!\n");
+            //for (int i = 0; i < tacoShoppingList.Count; i++)
+            //{
+            //    Console.WriteLine("Product: " + tacoShoppingList[i]);
+            //}
+
+
+            //tacoShoppingList.Add("Papas Fritas");
+            //tacoShoppingList.RemoveAt(1);
+            //Console.WriteLine("\nHello, Tacos JV!\n");
+            //for (int i = 0; i < tacoShoppingList.Count; i++)
+            //{
+            //    Console.WriteLine("Product: " + tacoShoppingList[i]);
+            //}
+
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------;
+
+            //Console.WriteLine("MÉTODOS");
+            //Console.WriteLine("");
+
+            //Random rnd = new Random();
+            //Console.WriteLine("Hello world, of random numbers! ");
+            //Console.WriteLine($"{rnd.Next(1, 10)}");
+
+            //Console.WriteLine("Hello world, of random numbers 2! ");
+            //Console.WriteLine($"{rnd.Next(1, 1000)}");
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------;
+
+            //Console.WriteLine("LA SENTENCIA (IF)");
+            //Console.WriteLine("");
+            //int anyValue = 14;
+            //string message = "" ;
+
+            //if (anyValue == 7)
+            //{
+            //    message = "OMG, It is a miracle anyValue is 7";
+            //}
+            //else if (anyValue == 14)
+            //{
+            //    message = "OMG, It is a double miracle anyValue is 14";
+            //}
+            //else 
+            //{
+            //    message = "Puff, the anyValue wasn't 7";
+            //}
+            //Console.WriteLine($"The answer is: {message}");
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------;
+
+            //Console.WriteLine("EXAMPLE OF IF IN A CALCULATOR");
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Hi! Welcome to C# Calculator");
+            //Console.WriteLine("");
+            //Console.WriteLine("Please, select your option: ");
+            //Console.WriteLine("1. Sum");
+            //Console.WriteLine("2. Substraction");
+            //Console.WriteLine("3. Multiplication");
+            //Console.WriteLine("4. Division");
+            //Console.WriteLine("");
+            //Console.WriteLine("Enter the number of your option: ");
+
+            //int option = int.Parse(Console.ReadLine());
+            //float result;
+            //if (option < 1 || option > 4)
+            //{
+            //    Console.WriteLine("You selected a incorrect option");
+            //    return;
+            //}
+            //Console.WriteLine("");
+            //Console.WriteLine("Write the first number: ");
+            //float numberOne = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("");
+            //Console.WriteLine("Write the second number: ");
+            //float numberTwo = float.Parse(Console.ReadLine());
+            //Console.WriteLine("");
+
+            //if (option == 1)
+            //{
+            //    result = numberOne + numberTwo;
+            //    Console.WriteLine($"The sum is: {result}");
+            //    Console.WriteLine("");
+            //}
+            //else if (option == 2)
+            //{
+            //    result = numberOne - numberTwo;
+            //    Console.WriteLine($"The substraction is: {result}");
+            //    Console.WriteLine("");
+            //}
+            //else if (option == 3)
+            //{
+            //    result = numberOne * numberTwo;
+            //    Console.WriteLine($"The multiplication is: {result}");
+            //    Console.WriteLine("");
+            //}
+            //else if (option == 4)
+            //{
+            //    result = numberOne / numberTwo;
+            //    Console.WriteLine($"The division is: {result}");
+            //    Console.WriteLine("");
+            //}
+            //Console.WriteLine("");
+            //Console.WriteLine("Thank you for using the C# calculator");
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------;
+
+            //Console.WriteLine("SETENCIA SWITCH");
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Plis selected the soda: ");
+            //string caseSwitch = Console.ReadLine();
+            //Console.WriteLine("");
+
+            //switch (caseSwitch)
+            //{
+            //    case "cola":
+            //        Console.WriteLine("Cola soda - $2 USD");
+            //        break;
+            //    case "lime":
+            //        Console.WriteLine("Lime soda - $1 USD");
+            //        break;
+            //    case "orange":
+            //        Console.WriteLine("Orange soda - $1.5 USD");
+            //        break;
+            //    case "apple":
+            //        Console.WriteLine("Apple soda - $1 USD");
+            //        break;
+            //    default:
+            //        Console.WriteLine("ERROR: You did not select a soda or you entered an incorrect value.");
+            //        break;
+            //}
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------;
+
+            //Console.WriteLine("CICLO FOR");
+            //Console.WriteLine("");
+
+            //for (int i = 0; i <=50; i+=10)
+            //{
+            //    Console.WriteLine("Hello world! #{0}", i);
+            //}
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------;
+
+            //Console.WriteLine("CICLO WHILE");
+            //Console.WriteLine("");
+
+            //bool continueSofwareExecution = false;
+            //do 
+            //{
+            //    Console.WriteLine("Do you wish to keep the sofware running? Write 1 if yes, Write 2 if no ");
+            //    int keepGoing = int.Parse(Console.ReadLine());
+
+            //    if (keepGoing == 1)
+            //    {
+            //        Console.WriteLine("Hello world! The sofware will keep running");
+            //        continueSofwareExecution = true;
+            //    }
+            //    else if (keepGoing == 0)
+            //    {
+            //        Console.WriteLine("This is last time the software is running");
+            //        continueSofwareExecution = false;
+            //    }
+            //    else
+            //        Console.WriteLine("Invalid input,Try again");      
+            //}while (continueSofwareExecution = false);
+            ////Console.WriteLine("-------------------------------------------------------------------");
+            ////Console.WriteLine("");
+            ////-------------------------------------------------------------------------------------------------------------------------;
+            
+            Console.WriteLine("LISTAS");
+            Console.WriteLine("");
+
+
+        }
+
+    }
+}
